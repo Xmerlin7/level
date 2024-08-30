c3d783c3f4b2dacb94320399bc61b7511eea76c4
Seif Allah <saifeldarageely@gmail.com>
2024-08-30 15:23:34 +0300

    Removing the addows

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,5 @@ $( function() {
       speed: 300,
       slidesToShow: 1,
       adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>'
     });
   } );
