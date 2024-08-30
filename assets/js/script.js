8a0b3a135fc0acde493195d2404c7696fd442bd4
Seif Allah <saifeldarageely@gmail.com>
2024-08-30 15:00:35 +0300

    Adding the slick J querry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 $( function() {
     $( "#inputCheckIn" ).datepicker();
     $( "#inputCheckOut" ).datepicker();
+    $('.level-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>'
+    });
   } );
