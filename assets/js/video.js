39b68eab136882ec6507b408f3b041beac9bc782
Seif Allah <saifeldarageely@gmail.com>
2024-08-30 19:05:29 +0300

    Toggling Video done

---
+++ assets/js/video.js
@@ -0,0 +1,22 @@
+$(function () {
+    function videoToggle() {
+        video = $('.Level-video').get(0)
+        if (video.paused) {
+            video.play();
+            $('.video-control-play').hide();
+            $('.video-control-pause').show();
+        }
+        else {
+            video.pause();
+            $('.video-control-pause').hide();
+            $('.video-control-play').show();
+        }
+    }
+    $('.video-control-play').click(function () {
+        videoToggle();
+    })
+    $('.video-control-pause').click(function () {
+        videoToggle();
+    })
+
+});
