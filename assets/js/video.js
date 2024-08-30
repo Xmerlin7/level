b486ec65bebdf85a694ab0b400ed81bc3075c316
Seif Allah <saifeldarageely@gmail.com>
2024-08-30 19:10:17 +0300

    Working Efficintly

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,22 +1,27 @@
 $(function () {
+    // Function to toggle video play/pause and control button visibility
     function videoToggle() {
-        video = $('.Level-video').get(0)
+        var video = $('.Level-video').get(0); // Get the video element
+        
+        // Check if the video is currently paused
         if (video.paused) {
-            video.play();
-            $('.video-control-play').hide();
-            $('.video-control-pause').show();
-        }
-        else {
-            video.pause();
-            $('.video-control-pause').hide();
-            $('.video-control-play').show();
+            video.play(); // Play the video
+            $('.video-control-play').hide(); // Hide the play button
+            $('.video-control-pause').show(); // Show the pause button
+        } else {
+            video.pause(); // Pause the video
+            $('.video-control-pause').hide(); // Hide the pause button
+            $('.video-control-play').show(); // Show the play button
         }
     }
-    $('.video-control-play').click(function () {
-        videoToggle();
-    })
-    $('.video-control-pause').click(function () {
-        videoToggle();
-    })
 
+    // Click event handler for the play button
+    $('.video-control-play').click(function () {
+        videoToggle(); // Call the videoToggle function to change video state
+    });
+
+    // Click event handler for the pause button
+    $('.video-control-pause').click(function () {
+        videoToggle(); // Call the videoToggle function to change video state
+    });
 });
